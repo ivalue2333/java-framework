825809e125427a585ea2745cd3cf59e0e79c297d
朱元兵 <1372254949@qq.com>
2020-09-04 10:37:27 +0800

    edit: some edit

---
+++ src/main/java/com/iota/core/data_structure/lfu/LFUCache.java
@@ -0,0 +1,103 @@
+package com.iota.core.data_structure.lfu;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
+
+// https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247486545&idx=1&sn=315ebfafa82c0dd3bcd9197eb270a7b6&chksm=9bd7f259aca07b4f063778509b3803993bc0d6cdaff32c076a102547b0afb82a5eea6119ed1a&mpshare=1&scene=1&srcid=0804v5nRqzuF7E44MKTsBurx&sharer_sharetime=1599018893659&sharer_shareid=2728d3a0eb6a32c660939921ae4b72ce&key=746aaa0a1205eb952695e34908f2f6dbeaf90d26a3ee60465177c2b1e642949ae3e25507049f4afcd45e823e3f3661824ee4dffb0928fc989ef53e8839cd421fea65b4308db66f84cc07fdec3567012b5a504566a1b73e74d434a928092cb1ba6b19a241498a9eac7a764ee3eb5aaf5edfd81f79518700eda8a5a64f9c3e220b&ascene=1&uin=MjQ3MzQ1MDUyNQ%3D%3D&devicetype=Windows+10+x64&version=62090529&lang=zh_CN&exportkey=A01WcG3dQHoeaZnBP3rNFK4%3D&pass_ticket=W37LZJj35wtgefD7LjgGM9gP1KY0TU1%2B6M4x0gG1h%2BHc5beiytDTWprI7S%2BS4dUf&wx_header=0
+class LFUCache {
+    // key 到 val 的映射，我们后文称为 KV 表
+    HashMap<Integer, Integer> keyToVal;
+    // key 到 freq 的映射，我们后文称为 KF 表
+    HashMap<Integer, Integer> keyToFreq;
+    // freq 到 key 列表的映射，我们后文称为 FK 表
+    HashMap<Integer, LinkedHashSet<Integer>> freqToKeys;
+    // 记录最小的频次
+    int minFreq;
+    // 记录 LFU 缓存的最大容量
+    int cap;
+
+    public LFUCache(int capacity) {
+        keyToVal = new HashMap<>();
+        keyToFreq = new HashMap<>();
+        freqToKeys = new HashMap<>();
+        this.cap = capacity;
+        this.minFreq = 0;
+    }
+
+    public int get(int key) {
+        if (!keyToVal.containsKey(key)) {
+            return -1;
+        }
+        // 增加 key 对应的 freq
+        increaseFreq(key);
+        return keyToVal.get(key);
+    }
+
+    public void put(int key, int val) {
+        if (this.cap <= 0) return;
+
+        /* 若 key 已存在，修改对应的 val 即可 */
+        if (keyToVal.containsKey(key)) {
+            keyToVal.put(key, val);
+            // key 对应的 freq 加一
+            increaseFreq(key);
+            return;
+        }
+
+        /* key 不存在，需要插入 */
+        /* 容量已满的话需要淘汰一个 freq 最小的 key */
+        if (this.cap <= keyToVal.size()) {
+            removeMinFreqKey();
+        }
+
+        /* 插入 key 和 val，对应的 freq 为 1 */
+        // 插入 KV 表
+        keyToVal.put(key, val);
+        // 插入 KF 表
+        keyToFreq.put(key, 1);
+        // 插入 FK 表
+        freqToKeys.putIfAbsent(1, new LinkedHashSet<>());
+        freqToKeys.get(1).add(key);
+        // 插入新 key 后最小的 freq 肯定是 1
+        this.minFreq = 1;
+    }
+
+    private void removeMinFreqKey() {
+        // freq 最小的 key 列表
+        LinkedHashSet<Integer> keyList = freqToKeys.get(this.minFreq);
+        // 其中最先被插入的那个 key 就是该被淘汰的 key
+        int deletedKey = keyList.iterator().next();
+        /* 更新 FK 表 */
+        keyList.remove(deletedKey);
+        if (keyList.isEmpty()) {
+            freqToKeys.remove(this.minFreq);
+            // 问：这里需要更新 minFreq 的值吗？
+        }
+        /* 更新 KV 表 */
+        keyToVal.remove(deletedKey);
+        /* 更新 KF 表 */
+        keyToFreq.remove(deletedKey);
+    }
+
+    private void increaseFreq(int key) {
+        int freq = keyToFreq.get(key);
+        /* 更新 KF 表 */
+        keyToFreq.put(key, freq + 1);
+        /* 更新 FK 表 */
+        // 将 key 从 freq 对应的列表中删除
+        freqToKeys.get(freq).remove(key);
+        // 将 key 加入 freq + 1 对应的列表中
+        freqToKeys.putIfAbsent(freq + 1, new LinkedHashSet<>());
+        freqToKeys.get(freq + 1).add(key);
+        // 如果 freq 对应的列表空了，移除这个 freq
+        if (freqToKeys.get(freq).isEmpty()) {
+            freqToKeys.remove(freq);
+            // 如果这个 freq 恰好是 minFreq，更新 minFreq
+            if (freq == this.minFreq) {
+                this.minFreq++;
+            }
+        }
+    }
+
+}
